ec008fee57c17b687e56f654b0fda8dee93d0660
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-02-22 14:51:52 -0300

    Ajuste no caça-palavras para dispositivos móveis

--- caca-palavras/script.js
+++ caca-palavras/script.js
@@ -45,10 +45,10 @@ const paintCell = (event) => {
 //função para gerar celulas dentro da matriz
 const generateCells = () => {
   const matriz = document.querySelector('.matriz');
-  for(let index = 0; index < 10; index += 1){
+  for(let index = 0; index < 7; index += 1){
     const line = document.createElement('div');
     line.className = 'line';
-    for(let index1 = 0; index1 < 10; index1 += 1){
+    for(let index1 = 0; index1 < 7; index1 += 1){
       const cell = document.createElement('div');
       cell.className = 'cell';
       cell.addEventListener('click', paintCell);
